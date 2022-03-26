58684045c176ec1a07d2a3ebd261f7e578ff828b
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-03-26 13:02:15 -0300

    feat: adding 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
   return (
-    <div>
-      Not Found!
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="font-bold text-9xl">
+        404
+      </h1>
+
+      <p className="text-2xl mb-5">
+        Page not found
+      </p>
+
+      <p className="text-base">
+        <Link to={'/'} title="Go home" className="text-blue-700 border-b border-blue-700 pb-px uppercase">
+          Go home
+        </Link>
+      </p>
     </div>
   )
 }
