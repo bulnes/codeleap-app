a70f5adf10385f7fcbdb8ceee854918d29d0f04f
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-03-26 12:44:48 -0300

    feature: adding route system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Main from './pages/Main'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
-    <BrowserRouter></BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='signup' element={<Signup />} />
+        <Route index element={<Main />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
